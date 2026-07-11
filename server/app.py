8035115229ee0557a2a0f1c899ec01b1361d4e70
ju2512 <jyothiurade874@gmail.com>
2026-07-11 19:49:24 +0530

    Added vercel url to app.py

--- server/app.py
+++ server/app.py
@@ -38,6 +38,7 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:3000",
         "http://localhost:3000",
+        "https://book-forge-dcs3ektzk-jyothi-urades-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
